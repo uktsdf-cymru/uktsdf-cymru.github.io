65008f3219129b89bcf5a3eaab93e24dd3fc03bd
Arwynor <142296765+Arwynor@users.noreply.github.com>
2025-01-01 09:08:40 +0000

    added social media icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './NavBar.css';
 import NavBarItem from './NavBarItem.js'
 import logo from '../tsd-c_logo.jpg';
+import { FaFacebook, FaYoutube, FaTiktok } from 'react-icons/fa'; // Import icons from react-icons
+
 
 function NavBar() {
     return (
@@ -14,6 +16,29 @@ function NavBar() {
                     <NavBarItem label="Student Library" action="/library" />
                     <NavBarItem label="Contact Us" action="/contact" />
                 </ul>
+                <div className="social-icons">
+                    <a
+                        href="https://www.facebook.com/regiononewales"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaFacebook size={24} />
+                    </a>
+                    <a
+                        href="https://www.youtube.com/@Tangsoodocymru"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaYoutube size={24} />
+                    </a>
+                    <a
+                        href="https://www.tiktok.com/@tsdcymru"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaTiktok size={24} />
+                    </a>
+                </div>
             </nav>
         </>
     );
