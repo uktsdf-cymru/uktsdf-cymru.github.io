b8dfda06ac4983bd954f079e0fc1d2d2c4b3bb0d
Arwynor <142296765+Arwynor@users.noreply.github.com>
2025-01-25 09:26:02 +0000

    Update Footer.js to fix warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 function Footer() {
     return (
         <footer className="App-footer">
-            <a href="https://uktsdf.org.uk/"><img src="https://uktsdf.org.uk/wp-content/uploads/2020/05/MDK-LOGO.png" width="64px" /></a>
+            <a href="https://uktsdf.org.uk/"><img src="https://uktsdf.org.uk/wp-content/uploads/2020/05/MDK-LOGO.png" width="64px" alt="Moo Duk Kwan Logo"/></a>
             <p>Proud to be part of the United Kingdom Tang Soo Do Federation</p>
             <p>Copyright &copy; 2025 Tang Soo Do Cymru</p>
         </footer>
